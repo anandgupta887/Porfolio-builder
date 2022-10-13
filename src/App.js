687886505fc08186dbbb820bc45c9b1e4be57543
Tejal <patiltejal439@gmail.com>
2022-10-13 15:55:24 +0530

    added header

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Home from "./Component/Home/Home";
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Home/>} />
-      </Routes>
+      <Switch>
+        <Route path="/"><Home /></Route>
+      </Switch>
     </Router>
   );
 }
