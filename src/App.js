63cc34b406d8d759d34dc702e58a61acadb4a4a7
Tejal <patiltejal439@gmail.com>
2022-10-18 01:39:03 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,14 +1,46 @@
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Home from "./Component/Home/Home";
+import profile from "./image/images.png";
+import email from "./image/image2.png";
+import pass from "./image/password.png";
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route path="/"><Home /></Route>
-      </Switch>
-    </Router>
+    <div className="main">
+      <div className="sub-main">
+        <div>
+          <div className="img">
+            <div className="container-image">
+              <img src={profile} alt="profile" />
+
+            </div>
+            </div>
+
+           <div>
+              <h1>Login Page</h1>
+            <div>
+              <img src={email} alt="email" className="email"/> 
+              <input type="text" placeholder="user name" className="name"/>
+             </div>
+             <div className="second-input">
+             <img src={pass} alt="pass" className="email"/>
+            <input type="text" placeholder="user name" className="name"/>
+            </div>
+            <div className="login-buttom">
+            <button>Login</button>
+            </div>
+
+  </div>
+        </div>
+      </div>
+    </div>
+    /*<Router>
+        <Switch>
+          <Route path="/"><Home /></Route>
+        </Switch>
+  </Router></>*/
+
   );
 }
 
--- src/Component/Home/Home.js
+++ src/Component/Home/Home.js
@@ -4,10 +4,10 @@ import Header from "./Header";
 function Home() {
   return (
     <>
+    
       <Header />
       <div style={{display:'flex'}}>
         <p>Hello</p>
-        <p>Bhsfhufhuyfhi</p>
       </div>
     </>
   );
