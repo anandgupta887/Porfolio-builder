b668a728e79254658b533a70a6d22d1cd45efa6f
Anand Gupta <anandgupta887@yahoo.com>
2022-10-29 13:34:42 +0530

    new login component added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Home from "./Component/Home/Home";
+import Login from "./Component/Login/Login";
 
 function App() {
   return (
     <Router>
       <Switch>
-        <Route path="/"><Home /></Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
       </Switch>
     </Router>
   );
---
+++ src/Component/Login/Login.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../../styles/Login.css'
+
+function Login() {
+  return (
+    <div>Login</div>
+  )
+}
+
+export default Login
