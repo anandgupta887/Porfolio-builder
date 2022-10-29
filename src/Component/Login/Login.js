f394980348f1139b0d4762d1c9681b42f4feec72
Tejal <patiltejal439@gmail.com>
2022-10-29 13:47:00 +0530

    Login page codeadded

--- src/Component/Login/Login.js
+++ src/Component/Login/Login.js
@@ -3,7 +3,38 @@ import '../../styles/Login.css'
 
 function Login() {
   return (
-    <div>Login</div>
+    <div className="main">
+      <div className="sub-main">
+        <div>
+          <div className="imgs">
+            <div className="container-image">
+              <img src="{profile}" alt="profile" className="profile" />
+
+            </div>
+            </div>
+
+           <div>
+              <h1>Login Page</h1>
+            <div>
+              <img src="{email}" alt="email" className="email"/> 
+              <input type="text" placeholder="user name" className="name"/>
+             </div> 
+             <div className="second-input">
+             <img src="{pass}" alt="pass" className="email"/>
+            <input type="password" placeholder="user name" className="name"/>
+            </div>
+            <div className="login-button">
+            <button>Login</button>
+            </div>
+            <p className="link">
+              <a href="#">Forgot password ?</a> Or <a href="#">Sign up</a>
+            </p>
+
+
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
