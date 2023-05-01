983be06a58f4561d8c90274d4fc3e72333f06194
priyajha <priyajha.tiim.18@gmail.com>
2023-05-01 11:59:23 +0530

    added some comments in Login.js

--- src/Component/Login/Login.js
+++ src/Component/Login/Login.js
@@ -31,9 +31,10 @@ function Login() {
   };
 
   // this is a state that will handle the state for visibility of snackbar
-  const [snackbarState, setSnackbarState] = useState(false);
-  //this is the state to owns the data to be visible n kind of severity it has - success or error
+  const [snackbarState, setSnackbarState] = useState(false); // "false" is the initial "snackbarState"
+  // this is the state to owns the data to be visible n kind of severity it has - success or error
   const [snackbarData, setSnackbarData] = useState({
+    // the following is the initial "snackbarData"
     message: "",
     severity: "",
   });
@@ -41,10 +42,11 @@ function Login() {
   const handleOpenSnackbar = (severity, message) => {
     // this function is setting the states
     setSnackbarData({
+      // changing initial/previous "snackbarState" to re-render the "Popup" component
       message: message,
       severity: severity,
     });
-    setSnackbarState(true);
+    setSnackbarState(true); // changing initial/previous "snackbarData" to re-render the "Popup" component
   };
 
   useEffect(() => {
