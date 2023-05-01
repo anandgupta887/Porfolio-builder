2d8170c466830dadd89ac908f3f6844da9a098f1
Anand Gupta <anandgupta887@yahoo.com>
2023-05-02 01:57:40 +0530

    fixes

--- src/Component/TemplateRoot/TemplateHandler.js
+++ src/Component/TemplateRoot/TemplateHandler.js
@@ -4,11 +4,18 @@ import { useParams } from "react-router-dom";
 import Template from "../Template/Template";
 import axios from "axios";
 import { backendUrl } from "../config/config";
+import CircularProgress from "@mui/material/CircularProgress";
+import { Backdrop, Box } from "@mui/material";
+import { useHistory } from "react-router-dom";
 
 function TemplateHandler({ browse }) {
   const [details, setDetails] = useState();
 
+  const [loader, setLoader] = useState(true);
+
   let { username } = useParams();
+
+  const history = useHistory()
 
   const userData = useSelector((state) => state?.user);
 
@@ -17,12 +24,19 @@ function TemplateHandler({ browse }) {
       try {
         await axios.get(`${backendUrl}/profiles/${username}`).then((res) => {
           setDetails(res.data.data);
+          setLoader(false);
         });
       } catch (err) {
         alert(err.response.data.error);
+        setLoader(false);
       }
     } else {
       setDetails(userData);
+      if (userData?.profile) {
+        setLoader(false);
+      } else {
+        history.push("/choose-template");
+      }
     }
   };
 
@@ -30,7 +44,20 @@ function TemplateHandler({ browse }) {
     loadDetails();
   }, []);
 
-  return <Template details={details} />;
+  return (
+    <>
+      {loader ? (
+        <Backdrop
+          sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 2 }}
+          open={loader}
+        >
+          <CircularProgress />
+        </Backdrop>
+      ) : (
+        <Template details={details} />
+      )}
+    </>
+  );
 }
 
 export default TemplateHandler;
